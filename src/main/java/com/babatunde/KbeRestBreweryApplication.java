4586923653e2ba7dc2b5d4d8c8b827fce3f3a0e2
Babatunde Raimi - Lawal <tuns13@yahoo.ca>
2024-11-04 16:13:34 +0000

    first commit

---
+++ src/main/java/com/babatunde/KbeRestBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.babatunde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeRestBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeRestBreweryApplication.class, args);
+	}
+
+}
